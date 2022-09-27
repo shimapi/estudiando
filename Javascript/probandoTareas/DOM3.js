54d17ddc741ee4c703c180d83fda736e99534427
shimapi <shirley.mallea2@gmail.com>
2022-09-27 18:35:36 -0300

    test

--- Javascript/probandoTareas/DOM3.js
+++ Javascript/probandoTareas/DOM3.js
@@ -18,9 +18,16 @@ function renderNoSongs() {
 
 function addSong(artistValue, titleValue) {
   const songTemplate = document.querySelector("#song-template").content;
-  const songElement = songTemplate.cloneNode(true);
-  console.log(songTemplate);
-  console.log(songElement);
+  const songElement = songTemplate.querySelector('.song').cloneNode(true);
+
+  songElement.querySelector(".song__artist").textContent = artistValue;
+  songElement.querySelector(".song__title").textContent = titleValue;
+  
+  songElement.querySelector(".song__like").addEventListener("click", function (evt) {
+    evt.target.classList.toggle("song__like_active");
+  });
+  
+  songsContainer.append(songElement);  
 }
 
 addButton.addEventListener("click", function () {
